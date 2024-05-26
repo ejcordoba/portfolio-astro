53bdcfc5e21f6957e50dbcc1a819db0a23af3acc
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-05-26 18:39:35 +0200

    Fix width small mobile and add custom breakpoint to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
+    screens: {
+      xs: "375px",
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         "complementary-color": "#bbee33",
