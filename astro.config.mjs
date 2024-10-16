23706aad3617c4b11a01d943de9d2bb167a73742
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-10-16 20:51:17 +0200

    add storyblok integration config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: "storyblok/BlogPost",
+      },
+      apiOptions: {
+        // Elige la región de tu espacio de Storyblok.
+        region: "eu", // opcional,  o 'eu' (por defecto)
+      },
+    }),
+  ],
   format: {
     enabled: false,
   },
